14cc55b18247e13f1364fc5f0d43cf16b72783e2
ubuntu_quinn <595money@gmail.com>
2022-04-17 22:14:06 +0800

    build: 加入spring boot

---
+++ src/main/java/com/quinn/performance/throughput/ThroughputHttpServer.java
@@ -0,0 +1,31 @@
+/**   
+* @Title ThroughputHttpServer.java 
+* @Package com.quinn.performance.throughput 
+* @Description TODO 
+* @author pigmilk
+* @date Apr 17, 2022 8:22:48 PM 
+* @version 1.0.0   
+*/
+package com.quinn.performance.throughput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+//import com.sun.httpserver.HttpServer;
+/**
+ * @ClassName ThroughputHttpServer
+ * @Description TODO(這裡用一句話描述這個類的作用)
+ * @author pigmilk
+ * @date Apr 17, 2022 8:22:48 PM
+ */
+
+public class ThroughputHttpServer {
+	private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+	public static void main(String[] args) throws IOException {
+		String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+		startServer(text);
+	}
+	public static void startServer(String text) {
+//		HttpServer server = new
+}
+}
