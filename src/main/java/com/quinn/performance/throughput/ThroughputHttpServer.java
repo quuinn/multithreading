7e3408c463627d22ee670625a805decc5493ba68
ubuntu_quinn <595money@gmail.com>
2022-04-18 07:20:31 +0800

    build(all): jdk 降版本, 17 >> 1.8
    為解決無聊的版本問題只好降版
    
    BREAKING CHANGE: change jdk version

--- src/main/java/com/quinn/performance/throughput/ThroughputHttpServer.java
+++ src/main/java/com/quinn/performance/throughput/ThroughputHttpServer.java
@@ -7,11 +7,12 @@
 * @version 1.0.0   
 */
 package com.quinn.performance.throughput;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import com.sun.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 /**
  * @ClassName ThroughputHttpServer
  * @Description TODO(這裡用一句話描述這個類的作用)
@@ -21,11 +22,13 @@ import java.nio.file.Paths;
 
 public class ThroughputHttpServer {
 	private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+
 	public static void main(String[] args) throws IOException {
 		String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
 		startServer(text);
 	}
-	public static void startServer(String text) {
-//		HttpServer server = new
-}
+
+	public static void startServer(String text) throws IOException {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+	}
 }
