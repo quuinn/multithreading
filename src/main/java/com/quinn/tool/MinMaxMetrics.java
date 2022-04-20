78d4e1b8600152bd5b47a777d0d76ff83e818590
ubuntu_quinn <595money@gmail.com>
2022-04-20 22:18:10 +0800

    feat: add MinMaxMetrics.java

---
+++ src/main/java/com/quinn/tool/MinMaxMetrics.java
@@ -0,0 +1,59 @@
+/**   
+* @Title MinMaxMetrics.java 
+* @Package com.quinn.tool 
+* @Description TODO 
+* @author pigmilk
+* @date Apr 20, 2022 10:02:32 PM 
+* @version 1.0.0   
+*/
+package com.quinn.tool;
+
+/**
+ * @ClassName MinMaxMetrics
+ * @Description TODO(這裡用一句話描述這個類的作用)
+ * @author pigmilk
+ * @date Apr 20, 2022 10:02:32 PM
+ */
+public class MinMaxMetrics {
+
+	// Add all necessary member variables
+	private volatile long min;
+	private volatile long max;
+
+	/**
+	 * Initializes all member variables
+	 */
+	public MinMaxMetrics() {
+		// Add code here
+		this.max = Long.MIN_VALUE;
+		this.min = Long.MAX_VALUE;
+	}
+
+	/**
+	 * Adds a new sample to our metrics.
+	 */
+	public void addSample(long newSample) {
+		// Add code here
+		synchronized (this) {
+			this.min = Math.min(newSample, this.min);
+			this.max = Math.max(newSample, this.max);
+		}
+	}
+
+	/**
+	 * Returns the smallest sample we've seen so far.
+	 */
+	public long getMin() {
+		// Add code here
+		return this.min;
+	}
+
+	/**
+	 * Returns the biggest sample we've seen so far.
+	 */
+	public long getMax() {
+		// Add code here
+		return this.max;
+	}
+
+}
